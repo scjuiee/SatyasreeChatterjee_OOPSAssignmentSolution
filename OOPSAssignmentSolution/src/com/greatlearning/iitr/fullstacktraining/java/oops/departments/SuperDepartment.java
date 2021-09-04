ab7070b9a7d893f556f22b374e471242e07f9a26
satyasreechatterjee <satyasreechatterjee@192.168.0.103>
2021-09-04 18:52:24 +0530

    format and indentation added

--- OOPSAssignmentSolution/src/com/greatlearning/iitr/fullstacktraining/java/oops/departments/SuperDepartment.java
+++ OOPSAssignmentSolution/src/com/greatlearning/iitr/fullstacktraining/java/oops/departments/SuperDepartment.java
@@ -4,24 +4,23 @@ public class SuperDepartment {
 
 	public String departmentName = "Super Department";
 	public String todayWork = "No Work as of now";
-	public String workDeadLine="Nil";
+	public String workDeadLine = "Nil";
 	public String holidayStr = "Today is not a holiday";
-	
+
 	public String getDepartmentName() {
 		return this.departmentName;
 	}
-	
+
 	public String getTodaysWork() {
 		return this.todayWork;
 	}
-	
+
 	public String getWorkDeadline() {
 		return this.workDeadLine;
 	}
-	
-    public String isTodayAHoliday() {
-    	return this.holidayStr;
-    }
-    
+
+	public String isTodayAHoliday() {
+		return this.holidayStr;
+	}
 
 }
--- OOPSAssignmentSolution/src/com/greatlearning/iitr/fullstacktraining/java/oops/departments/TechDepartment.java
+++ OOPSAssignmentSolution/src/com/greatlearning/iitr/fullstacktraining/java/oops/departments/TechDepartment.java
@@ -1,27 +1,27 @@
 package com.greatlearning.iitr.fullstacktraining.java.oops.departments;
 
-public class TechDepartment extends SuperDepartment{
+public class TechDepartment extends SuperDepartment {
 	public String departmentName = "Hr Department";
 	public String todayWork = "Complete coding of module 1";
-	public String workDeadLine="Complete by EOD ";
-	public String techStackInformation="Core Java";
-	
+	public String workDeadLine = "Complete by EOD ";
+	public String techStackInformation = "Core Java";
+
 	public TechDepartment() {
 		System.out.println("Welcome to Tech Department");
 	}
-	
+
 	public String getDepartmentName() {
 		return this.departmentName;
 	}
-	
+
 	public String getTodaysWork() {
 		return this.todayWork;
 	}
-	
+
 	public String getWorkDeadline() {
 		return this.workDeadLine;
 	}
-	
+
 	public String getTechStackInformation() {
 		return this.techStackInformation;
 	}
